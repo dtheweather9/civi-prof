d9ab981c0663bc58f52a37e41abdcc7f9cc18552
dtheweather9 <dmpastuf@gmail.com>
2013-04-08 16:26:03 -0300

    Update profileins.php
    Updated comment section

--- profileins.php
+++ profileins.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Buddypress Civicrm Syncs
-Plugin URI: http://www.danpastuf.com
+Plugin URI: http://github.com/dtheweather9/civi-prof/
 Description: A plugin to connect Civicrm and Buddypress
 Version: 0.01
 Author: Dan Pastuf
